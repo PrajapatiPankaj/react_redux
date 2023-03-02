7ea33dff9dc58620739ae3d20344ecea6455c795
PankajPrajapati <paprajapati.25.1@gmail.com>
2023-03-02 16:31:22 +0530

    card shoed in proper manner

--- src/container/ProductComponent.js
+++ src/container/ProductComponent.js
@@ -7,22 +7,24 @@ const ProductComponent = () => {
 
     const renderList = products.map((product) => {
           const  {id, price,category,title,image} = product ;
-         return(
-            <div className="four column wide" key={id} >
-                <div className="ui cards">
-                    <div className="card" >
-                        <div className="image">
-                            <img src={image} alt={title} />    
-                         </div>
-                          <div className="content">
-                            <div className="header">{title}</div> 
-                            <div className="meta price">$ {price} </div>
-                            <div className="meta">{category}</div>
-                         </div>
-                    </div>
-                </div>
-            </div>
-     );
+         return (
+           <div className="ui grid">
+             <div className="ui container four column wide" key={id}>
+               <div className="ui cards">
+                 <div className="card">
+                   <div className="image">
+                     <img src={image} alt={title} />
+                   </div>
+                   <div className="content">
+                     <div className="header">{title}</div>
+                     <div className="meta price">$ {price} </div>
+                     <div className="meta">{category}</div>
+                   </div>
+                 </div>
+               </div>
+             </div>
+           </div>
+         );
               
          })
   
